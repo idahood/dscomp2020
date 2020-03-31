0e2b1174007fd371906b3c902be4c89ebb9e0219
Steve Dodd <sdodd@salesforce.com>
2020-03-30 20:27:56 -0600

    Reduce entropy

--- hao_model.py
+++ hao_model.py
@@ -47,10 +47,10 @@ def main():
             featurewise_std_normalization=False,  # divide inputs by std of the dataset
             samplewise_std_normalization=False,  # divide each input by its std
             zca_whitening=False,  # apply ZCA whitening
-            rotation_range=15,  # randomly rotate images in the range (degrees, 0 to 180)
-            zoom_range = 0.25, # Randomly zoom image
-            width_shift_range=0.25,  # randomly shift images horizontally (fraction of total width)
-            height_shift_range=0.25,  # randomly shift images vertically (fraction of total height)
+            rotation_range=10,  # randomly rotate images in the range (degrees, 0 to 180)
+            zoom_range = 0.1, # Randomly zoom image
+            width_shift_range=0.2,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.2,  # randomly shift images vertically (fraction of total height)
             horizontal_flip=False,  # randomly flip images
             vertical_flip=False)  # randomly flip images
 
