2c2bd823691079a42418da49df0875632075703d
Steve Dodd <sdodd@salesforce.com>
2020-03-30 19:49:21 -0600

    Increase size of conv layers

--- my_model.py
+++ my_model.py
@@ -45,11 +45,11 @@ def main():
     y_train = keras.utils.to_categorical(y_train, num_classes)
 
     model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3),
+    model.add(Conv2D(32, kernel_size=(7, 7),
                      activation='relu',
                      input_shape=input_shape))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, (7, 7), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(6, 6)))
     model.add(Dropout(0.25))
     model.add(Flatten())
     model.add(Dense(128, activation='relu'))
